9fc27e36a46eac87d8eb61374ddc528cb6b46739
Tito Miguel Costa <titomiguelcosta@gmail.com>
2018-03-27 14:54:04 +1300

    support for ebs

---
+++ src/Controller/StaticController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * @package App\Controller
+ */
+class StaticController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function home()
+    {
+        return $this->json(['version' => 'v1']);
+    }
+}
