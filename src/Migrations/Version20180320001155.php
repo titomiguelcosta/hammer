f2ded4ae3d5f072c48d1efb586e93446190a5f0a
Tito Miguel Costa <titomiguelcosta@gmail.com>
2018-04-01 10:40:11 +1200

    update migrations to support sqlite

--- src/Migrations/Version20180320001155.php
+++ src/Migrations/Version20180320001155.php
@@ -12,17 +12,16 @@ class Version20180320001155 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        //$this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('CREATE TABLE oauth (id INT AUTO_INCREMENT NOT NULL, service VARCHAR(255) NOT NULL, access_token VARCHAR(1024) NOT NULL, refresh_token VARCHAR(1024) NOT NULL, expires_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $platform = $this->connection->getDatabasePlatform()->getName();
+        if ('mysql' === $platform) {
+            $this->addSql('CREATE TABLE oauth (id INT AUTO_INCREMENT NOT NULL, service VARCHAR(255) NOT NULL, access_token VARCHAR(1024) NOT NULL, refresh_token VARCHAR(1024) NOT NULL, expires_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        } elseif ('sqlite' === $platform) {
+            $this->addSql('CREATE TABLE oauth (id INTEGER NOT NULL, service VARCHAR(255) NOT NULL, access_token VARCHAR(1024) NOT NULL, refresh_token VARCHAR(1024) NOT NULL, expires_at DATETIME NOT NULL, PRIMARY KEY(id))');
+        }
     }
 
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        //$this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
         $this->addSql('DROP TABLE oauth');
     }
 }
