685ea6af10888a183fe7a757ac57ac81e9d4bfbe
Tito Miguel Costa <titomiguelcosta@gmail.com>
2019-11-30 00:19:35 +0100

    fix deploy script

--- deploy.php
+++ deploy.php
@@ -9,7 +9,7 @@ set('repository', 'https://github.com/titomiguelcosta/hammer.git');
 set('git_tty', false);
 set('keep_releases', 3);
 set('shared_dirs', ['var/log', 'var/sessions', 'vendor']);
-set('writable_dirs', ['var']);
+set('writable_dirs', ['var', 'var/cache']);
 set('composer_action', 'install');
 set('composer_options', '{{composer_action}} --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader --no-suggest');
 
@@ -18,7 +18,7 @@ host('api.hammer.titomiguelcosta.com')
     ->stage('prod')
     ->set('deploy_path', '/mnt/websites/hammer')
     ->set('shared_files', ['.env.prod.local'])
-    ->set('http_user', 'ubuntu')
+    ->set('http_user', 'www-data')
     ->set('writable_mode', 'acl')
     ->set('branch', 'master')
     ->set('env', ['APP_ENV' => 'prod']);
